618a4e9aa01bbef46a6efc24096e8c5a89931ac5
saicharan2222 <109336035+saicharan2222@users.noreply.github.com>
2022-08-16 16:45:19 +0530

    Update Python_file.py

--- Python_file.py
+++ Python_file.py
@@ -1,7 +1,16 @@
-a=5
-b=5
-print(a+b)
-print(a-b)
-print(a/b)
-print(a*b)
-print(a//b)
+# Python program to swap two variables
+
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
